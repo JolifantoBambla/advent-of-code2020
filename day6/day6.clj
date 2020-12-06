7a1e0ea1076dd65e40e4b76f4e0003e52c72054e
Lukas Herzberger <herzberger.lukas@gmail.com>
2020-12-06 14:52:07 +0100

    clean-up day6

--- day6/day6.clj
+++ day6/day6.clj
@@ -1,5 +1,26 @@
-(require '[clojure.string :as str])
-(require '[clojure.set :as set])
+(ns day6
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def example-data
+  "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b")
+
+(def puzzle-input
+  (slurp "input.txt"))
 
 (defn count-distinct-chars
   [string]
@@ -25,43 +46,30 @@
   [groups]
   (map count-distinct-chars groups))
 
-(def example-data
-  "abc
-
-a
-b
-c
-
-ab
-ac
-
-a
-a
-a
-a
-
-b")
-
-
-(defn part1
-  [data]
-  (reduce + (groups->distinct-char-counts (extract-groups data remove-newlines))))
-
-(def puzzle-input
-  (slurp "input.txt"))
-
 (defn string->sets
   [string]
   (map set (split-newline string)))
 
-(defn sets->intersection-counts
+(defn sets->intersection-count
   [sets]
   (count (reduce set/intersection sets)))
 
 (defn groups->intersection-counts
   [groups]
-  (map sets->intersection-counts groups))
+  (map sets->intersection-count groups))
+
+(defn part1
+  [data]
+  (reduce + (groups->distinct-char-counts (extract-groups data remove-newlines))))
 
 (defn part2
   [data]
   (reduce + (groups->intersection-counts (extract-groups data string->sets))))
+
+(defn -main
+  [& args]
+  (println "Part 1 (example):" (part1 example-data))
+  (println "Part 2 (example):" (part2 example-data))
+  (println "Part 1:" (part1 puzzle-input))
+  (println "Part 2:" (part2 puzzle-input)))
+
